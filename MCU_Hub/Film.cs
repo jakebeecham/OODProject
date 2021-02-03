538d128ffc20d7cf7544c4cebc7428e82c101492
Jake Beecham <60218222+JakeBeecham@users.noreply.github.com>
2021-02-03 13:11:18 +0000

    OODProject Project Class Created.

---
+++ MCU_Hub/Film.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCU_Hub
+{
+    public class Film : Project
+    {
+        #region Properties
+
+        public string Director { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public Film(string title, DateTime releaseDate, byte duration, string rating, string director, string cast, PhaseType phase) :
+            base(title, releaseDate, duration, rating, cast, phase)
+        {
+            Director = director;
+        }
+
+        public Film(string title, DateTime releaseDate, byte duration) :
+            this(title, releaseDate, duration, "0", "Unknown", "Unknown", PhaseType.Zero)
+        { }
+
+        #endregion
+    }
+}
---
+++ MCU_Hub/Show.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCU_Hub
+{
+    public class Show : Project
+    {
+        #region Properties
+
+        public string Creator { get; set; }
+
+        public byte NumberOfEpisodes { get; set; }
+
+        #endregion
+    }
+}
