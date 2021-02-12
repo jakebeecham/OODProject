6734fc669ea0e7584b105b9e1270a9ab37758f9c
Jake Beecham <60218222+JakeBeecham@users.noreply.github.com>
2021-02-12 20:25:41 +0000

    OODProject Setup Code Started.

--- MCU_Hub/MainWindow.xaml.cs
+++ MCU_Hub/MainWindow.xaml.cs
@@ -20,9 +20,48 @@ namespace MCU_Hub
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Setup
+
+        List<Project> allProjects = new List<Project>();
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Populate Combo Box
+            string[] filterTypes = { "Release Order", "Chronological Order", "Phase One", "Phase Two", "Phase Three", "Phase Four" };
+            cbBoxFilter.ItemsSource = filterTypes;
+            cbBoxFilter.SelectedItem = "Release Order";
+
+            //Creating All Projects
+            Film film1 = new Film("Iron Man", new DateTime(2008, 5, 2), 127, "12+", "Jon Favreau", "Robert Downey Jr., Gwyneth Paltrow", Project.PhaseType.One);
+            //Film film2 = new Film("Iron Man", new DateTime(2008, 5, 2), 127, "12+", "Jon Favreau", "", Project.PhaseType.One);
+            //Film film3 = new Film("Iron Man", new DateTime(2008, 5, 2), 127, "12+", "Jon Favreau", "", Project.PhaseType.One);
+            //Film film4 = new Film("Iron Man", new DateTime(2008, 5, 2), 127, "12+", "Jon Favreau", "", Project.PhaseType.One);
+            //Film film5 = new Film("Iron Man", new DateTime(2008, 5, 2), 127, "12+", "Jon Favreau", "", Project.PhaseType.One);
+            //Film film6 = new Film("Iron Man", new DateTime(2008, 5, 2), 127, "12+", "Jon Favreau", "", Project.PhaseType.One);
+            //Film film7 = new Film("Iron Man", new DateTime(2008, 5, 2), 127, "12+", "Jon Favreau", "", Project.PhaseType.One);
+            //Film film8 = new Film("Iron Man", new DateTime(2008, 5, 2), 127, "12+", "Jon Favreau", "", Project.PhaseType.One);
+            //Film film9 = new Film("Iron Man", new DateTime(2008, 5, 2), 127, "12+", "Jon Favreau", "", Project.PhaseType.One);
+            //Film film10 = new Film("Iron Man", new DateTime(2008, 5, 2), 127, "12+", "Jon Favreau", "", Project.PhaseType.One);
+            //Film film11 = new Film("Iron Man", new DateTime(2008, 5, 2), 127, "12+", "Jon Favreau", "", Project.PhaseType.One);
+            //Film film12 = new Film("Iron Man", new DateTime(2008, 5, 2), 127, "12+", "Jon Favreau", "", Project.PhaseType.One);
+            //Film film13 = new Film("Iron Man", new DateTime(2008, 5, 2), 127, "12+", "Jon Favreau", "", Project.PhaseType.One);
+            //Film film14 = new Film("Iron Man", new DateTime(2008, 5, 2), 127, "12+", "Jon Favreau", "", Project.PhaseType.One);
+            //Film film15 = new Film("Iron Man", new DateTime(2008, 5, 2), 127, "12+", "Jon Favreau", "", Project.PhaseType.One);
+            //Film film16 = new Film("Iron Man", new DateTime(2008, 5, 2), 127, "12+", "Jon Favreau", "", Project.PhaseType.One);
+            //Film film17 = new Film("Iron Man", new DateTime(2008, 5, 2), 127, "12+", "Jon Favreau", "", Project.PhaseType.One);
+            //Film film18 = new Film("Iron Man", new DateTime(2008, 5, 2), 127, "12+", "Jon Favreau", "", Project.PhaseType.One);
+            //Film film19 = new Film("Iron Man", new DateTime(2008, 5, 2), 127, "12+", "Jon Favreau", "", Project.PhaseType.One);
+            //Film film20 = new Film("Iron Man", new DateTime(2008, 5, 2), 127, "12+", "Jon Favreau", "", Project.PhaseType.One);
+            //Film film21 = new Film("Iron Man", new DateTime(2008, 5, 2), 127, "12+", "Jon Favreau", "", Project.PhaseType.One);
+            //Film film22 = new Film("Iron Man", new DateTime(2008, 5, 2), 127, "12+", "Jon Favreau", "", Project.PhaseType.One);
+
+        }
+
+        #endregion
     }
 }
